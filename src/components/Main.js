0255af953cd665d001f375a88d7d10473d34f44c
Martin Daniel <martindaniel555@gmail.com>
2020-04-18 19:39:43 +0300

    Add loading screen and refresh button

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,8 @@ import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import SyncIcon from '@material-ui/icons/Sync';
+import IconButton from '@material-ui/core/IconButton';
 import { fetchAll, fetchLocation } from 'actions';
 import useStyles from 'assets/styles';
 import Table from 'components/Table';
@@ -34,15 +36,25 @@ const Main = (props) => {
   return (
     <main className={classes.content}>
       <div className={classes.appBarSpacer} />
-      <Container maxWidth='lg' className={classes.container}>
-        <Backdrop open={props.isLoading}>
-          <CircularProgress />
-        </Backdrop>
-        {!props.isLoading ? <Table /> : ''}
-        <Box pt={4}>
-          <Copyright />
-        </Box>
-      </Container>
+      <Backdrop open={props.isLoading}>
+        <CircularProgress />
+      </Backdrop>
+      {!props.isLoading ? (
+        <Container maxWidth='lg' className={classes.container}>
+          <Box>
+            <IconButton onClick={() => props.fetchAll()}>
+              <SyncIcon color='primary' />
+            </IconButton>
+          </Box>
+          <Table />
+
+          <Box pt={4}>
+            <Copyright />
+          </Box>
+        </Container>
+      ) : (
+        ''
+      )}
     </main>
   );
 };
